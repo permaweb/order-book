19cf53b34c9a5cf858f866f4da58737d181cafa4
Justin Shaw <mogulx_operates@yahoo.com>
2023-06-06 08:04:28 -0500

    chore: format

--- lib/src/helpers/config.ts
+++ lib/src/helpers/config.ts
@@ -1,7 +1,5 @@
-
-
-export const ORDERBOOK_CONTRACT = "hY3jZrvejIjQmLjya3yarDyKNgdiG-BiR6GxG_X3rY8";
+export const ORDERBOOK_CONTRACT = 'hY3jZrvejIjQmLjya3yarDyKNgdiG-BiR6GxG_X3rY8';
 
 export const CURRENCY_DICT = {
-	U: "IH-NaCRpqneQJVTXKQfvYHd07RJ5zYhebU4UuBv_Dcs",
-}
+	U: 'IH-NaCRpqneQJVTXKQfvYHd07RJ5zYhebU4UuBv_Dcs',
+};
