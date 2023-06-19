7858e1352a22d81b083c2744e8c5b4221e612537
NickJ202 <nickjuliano20@gmail.com>
2023-06-19 11:23:25 -0400

    seperate endpoints

--- app/src/providers/ArweaveProvider.tsx
+++ app/src/providers/ArweaveProvider.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { getArweaveBalanceEndpoint } from 'permaweb-orderbook';
-
 import { Modal } from 'components/molecules/Modal';
 import { AR_WALLETS, WALLET_PERMISSIONS } from 'helpers/config';
+import { getArweaveBalanceEndpoint } from 'helpers/endpoints';
 import { language } from 'helpers/language';
 import { STYLING } from 'helpers/styling';
 
--- app/src/views/Landing/index.tsx
+++ app/src/views/Landing/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-import * as OrderBook from 'permaweb-orderbook';
+import { AssetType, getAssetsByContract } from 'permaweb-orderbook';
 
 import { AssetsGrid } from 'global/AssetsGrid';
 import { AssetsList } from 'global/AssetsList';
 import { FEATURE_COUNT } from 'helpers/config';
 
 export default function Landing() {
-	const [assets, setAssets] = React.useState<OrderBook.AssetType[] | null>(null);
+	const [assets, setAssets] = React.useState<AssetType[] | null>(null);
 
-	const [featuredAssets, setFeaturedAssets] = React.useState<OrderBook.AssetType[] | null>(null);
-	const [remainingAssets, setRemainingAssets] = React.useState<OrderBook.AssetType[] | null>(null);
+	const [featuredAssets, setFeaturedAssets] = React.useState<AssetType[] | null>(null);
+	const [remainingAssets, setRemainingAssets] = React.useState<AssetType[] | null>(null);
 
 	React.useEffect(() => {
 		(async function () {
-			setAssets(await OrderBook.getAssetsByContract());
+			setAssets(await getAssetsByContract());
 		})();
 	}, []);
 
@@ -24,13 +24,12 @@ export default function Landing() {
 			if (assets.length >= FEATURE_COUNT) {
 				setFeaturedAssets(assets.slice(0, FEATURE_COUNT));
 				setRemainingAssets(assets.slice(FEATURE_COUNT));
-			}
-			else {
+			} else {
 				setFeaturedAssets(assets);
 				setRemainingAssets([]);
 			}
 		}
-	}, [assets])
+	}, [assets]);
 
 	function getFeaturedAssets() {
 		if (featuredAssets) {
