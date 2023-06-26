7c00b03aae485de548f2392d08dd6e5519a29e04
Vince Juliano <v.juliano6363@gmail.com>
2023-06-26 09:11:38 -0400

    fix paginator var

--- sdk/src/helpers/endpoints.ts
+++ sdk/src/helpers/endpoints.ts
@@ -1,7 +1,7 @@
 import { PAGINATOR } from './config';
 
 export function getBalancesEndpoint(walletAddress: string) {
-	return `https://contracts.warp.cc/balances?walletAddress=${walletAddress}&limit=${1000}`;
+	return `https://contracts.warp.cc/balances?walletAddress=${walletAddress}&limit=${PAGINATOR}`;
 }
 
 export function getTxEndpoint(txId: string) {
