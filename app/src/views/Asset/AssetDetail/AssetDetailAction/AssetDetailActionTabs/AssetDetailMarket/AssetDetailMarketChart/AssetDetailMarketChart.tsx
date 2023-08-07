96b9b3d8f0e35195c73a8715c5f7194612f53bf3
NickJ202 <nickjuliano20@gmail.com>
2023-08-07 12:22:34 -0400

    style update

--- app/src/views/Asset/AssetDetail/AssetDetailAction/AssetDetailActionTabs/AssetDetailMarket/AssetDetailMarketChart/AssetDetailMarketChart.tsx
+++ app/src/views/Asset/AssetDetail/AssetDetailAction/AssetDetailActionTabs/AssetDetailMarket/AssetDetailMarketChart/AssetDetailMarketChart.tsx
@@ -88,7 +88,7 @@ export default function AssetDetailMarketChart(props: IProps) {
 				<S.ChartKeyWrapper>
 					{sortedOwners.map((owner: OwnerType | OwnerListingType, index: number) => {
 						return (
-							<S.ChartKeyLine key={index} first={index === 0}>
+							<S.ChartKeyLine key={index} first={index === 0 && owner.address === ORDERBOOK_CONTRACT}>
 								<S.ChartKey background={keys[index] ? keys[index] : theme.colors.stats.alt9} />
 								{owner.address !== ORDERBOOK_CONTRACT ? (
 									<>
